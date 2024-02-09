29daf60f754e7d40745b1cd662e40b30eaf6537e
Ottman <133713821+Otmanbboy@users.noreply.github.com>
2024-02-09 23:00:52 +0100

    Create 4-hbnb.js

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,31 @@
+$(function () {
+  const amenityIds = {};
+  $('.amenities .popover li input').change(function (event) {
+    if (this.checked) {
+      amenityIds[this.dataset.id] = this.dataset.name;
+    } else {
+      delete amenityIds[this.dataset.id];
+    }
+    const names = Object.values(amenityIds);
+    $('.amenities h4').text(names.join(', '));
+  });
+  $('.filters button').click(function (event) {
+    $.ajax({
+      url: 'http://0.0.0.0:5001/api/v1/places_search',
+      type: 'POST',
+      contentType: 'application/json',
+      dataType: 'JSON',
+      data: JSON.stringify({ amenities: Object.keys(amenityIds) }),
+      success: function (data) {
+        let i;
+        let newHTML = [];
+        for (i = 0; i < data.length; i++) {
+          newHTML.push(createHTML(data[i]));
+        }
+        newHTML = newHTML.join('');
+        $('section.places > article').remove();
+        $('section.places').append(newHTML);
+      }
+    });
+  });
+  
