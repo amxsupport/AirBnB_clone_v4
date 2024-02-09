44ce656eb760d5b3c2b7175b4407806fb7bafb7e
Ottman <133713821+Otmanbboy@users.noreply.github.com>
2024-02-09 22:58:11 +0100

    Update 4-hbnb.py

--- web_dynamic/4-hbnb.py
+++ web_dynamic/4-hbnb.py
@@ -24,3 +24,26 @@ def teardown_db(exception):
     storage.close()
 
 
+@app.route('/4-hbnb')
+def hbnb_filters(the_id=None):
+    """
+    handles request to custom template with states, cities & amentities
+    """
+    state_objs = storage.all('State').values()
+    states = dict([state.name, state] for state in state_objs)
+    amens = storage.all('Amenity').values()
+    places = storage.all('Place').values()
+    users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
+                 for user in storage.all('User').values())
+    cache_id = uuid.uuid4()
+    return render_template('4-hbnb.html',
+                           states=states,
+                           amens=amens,
+                           places=places,
+                           users=users,
+                           cache_id=cache_id)
+
+if __name__ == "__main__":
+    """
+    MAIN Flask App"""
+    app.run(host=host, port=port)
