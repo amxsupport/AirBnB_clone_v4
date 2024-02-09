7f47b942d089bee8598a53830dc54d5e46e20d70
Ottman <133713821+Otmanbboy@users.noreply.github.com>
2024-02-09 23:02:24 +0100

    Update 4-hbnb.js

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -51,3 +51,57 @@ $(function () {
       }
     }
   });
+  function createHTML (place) {
+    return (
+      `<article>
+
+        <div class="title">
+
+        <h2>${place.name}</h2>
+
+        <div class="price_by_night" style="min-width: 70px">
+
+      $${place.price_by_night}
+
+          </div>
+        </div>
+        <div class="information">
+          <div class="max_guest">
+        <i class="fa fa-users fa-3x" aria-hidden="true"></i>
+
+        <br />
+
+        ${place.max_guest} Guests
+
+          </div>
+          <div class="number_rooms">
+        <i class="fa fa-bed fa-3x" aria-hidden="true"></i>
+
+        <br />
+
+        ${place.number_rooms} Bedrooms
+          </div>
+          <div class="number_bathrooms">
+        <i class="fa fa-bath fa-3x" aria-hidden="true"></i>
+
+        <br />
+
+        ${place.number_bathrooms} Bathroom
+
+          </div>
+        </div>
+
+        <div class="user">
+
+
+        </div>
+
+        <div class="description">
+
+          ${place.description}
+
+      </div>
+
+      </article>`);
+  }
+});
