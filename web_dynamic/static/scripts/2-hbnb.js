94825d4fa011757d756e7ed85ba646f2a025ecd7
amxsupport <itmasterdesigne@gmail.com>
2024-02-09 21:43:46 +0000

    create static/scripts/2-hbnb.js

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,23 @@
+$(function () {
+  const amenityIds = {};
+  $('.amenities .popover li input').change(function (event) {
+    if (this.checked) {
+      amenityIds[this.dataset.id] = this.dataset.name;
+    } else {
+      delete amenityIds[this.dataset.id];
+    }
+    const names = Object.values(amenityIds);
+    $('.amenities h4').text(names.join(', '));
+  });
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/status/',
+    success: function (data, textStatus) {
+      console.log(data.status);
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+      }
+    }
+  });
+});
