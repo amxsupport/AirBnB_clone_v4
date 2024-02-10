6955c952b080cdf3980bd36ecbbeb36a18c65848
Ottman <133713821+Otmanbboy@users.noreply.github.com>
2024-02-10 13:28:53 +0100

    Update 100-hbnb.js

--- web_dynamic/static/scripts/100-hbnb.js
+++ web_dynamic/static/scripts/100-hbnb.js
@@ -23,3 +23,62 @@ $(function () {
     cityNames = names.join(', ');
     updateH4();
   });
+  function updateH4 () {
+    if (stateNames.length > 0 && cityNames.length > 0) {
+      $(' .locations h4').text(stateNames + ', ' + cityNames);
+    } else {
+      $(' .locations h4').text(stateNames + cityNames);
+    }
+  }
+  const amenityIds = {};
+  $('.amenities .popover li input').change(function (event) {
+    if (this.checked) {
+      amenityIds[this.dataset.id] = this.dataset.name;
+    } else {
+      delete amenityIds[this.dataset.id];
+    }
+    const names = Object.values(amenityIds);
+    $('.amenities h4').text(names.join(', '));
+  });
+  $('.filters button').click(function (event) {
+    $.ajax({
+      url: 'http://0.0.0.0:5001/api/v1/places_search',
+      type: 'POST',
+      contentType: 'application/json',
+      dataType: 'JSON',
+      data: JSON.stringify({ amenities: Object.keys(amenityIds), states: Object.keys(stateIds), cities: Object.keys(cityIds) }),
+      success: function (data) {
+        let i;
+        let newHTML = [];
+        for (i = 0; i < data.length; i++) {
+          newHTML.push(createHTML(data[i]));
+        }
+        newHTML = newHTML.join('');
+        $('section.places > article').remove();
+        $('section.places').append(newHTML);
+      }
+    });
+  });
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/status/',
+    success: function (data) {
+      console.log(data.status);
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+      }
+    }
+  });
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/places_search',
+    type: 'POST',
+    contentType: 'application/json',
+    data: '{}',
+    success: function (data) {
+      let i;
+      for (i = 0; i < data.length; i++) {
+        $('section.places').append(createHTML(data[i]));
+      }
+    }
+  });
