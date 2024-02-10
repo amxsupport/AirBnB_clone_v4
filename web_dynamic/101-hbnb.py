ad90b42f1553e28fc0025ced09f2dfe16a0b5c5c
Ottman <133713821+Otmanbboy@users.noreply.github.com>
2024-02-10 13:37:42 +0100

    Update 101-hbnb.py implement teardown_db def exeption

--- web_dynamic/101-hbnb.py
+++ web_dynamic/101-hbnb.py
@@ -12,3 +12,14 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 port = 5000
 host = '0.0.0.0'
+
+
+# begin flask page rendering
+@app.teardown_appcontext
+def teardown_db(exception):
+    """
+    after each request, this method calls .close() (i.e. .remove()) on
+    the current SQLAlchemy Session
+    """
+    storage.close()
+
