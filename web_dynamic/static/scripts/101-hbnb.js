2c2e348ebf42428adeb21f119b07f1863136a840
Ottman <133713821+Otmanbboy@users.noreply.github.com>
2024-02-10 13:50:26 +0100

    Update 101-hbnb.js

--- web_dynamic/static/scripts/101-hbnb.js
+++ web_dynamic/static/scripts/101-hbnb.js
@@ -11,3 +11,18 @@ $(function () {
     stateNames = (names.join(', '));
     updateH4();
   });
+  
+  const cityIds = {};
+  let cityNames = '';
+  $('.cities input').change(function (event) {
+    if (this.checked) {
+      cityIds[this.dataset.id] = this.dataset.name;
+    } else {
+      delete cityIds[this.dataset.id];
+    }
+    const names = Object.values(cityIds);
+    cityNames = names.join(', ');
+    updateH4();
+  });
+
+  
