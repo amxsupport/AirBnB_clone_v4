f3dc8784fc3ee0f93c565e99a3e9397e7af097f6
Ottman <133713821+Otmanbboy@users.noreply.github.com>
2024-02-10 13:51:45 +0100

    Update 101-hbnb.js

--- web_dynamic/static/scripts/101-hbnb.js
+++ web_dynamic/static/scripts/101-hbnb.js
@@ -25,4 +25,20 @@ $(function () {
     updateH4();
   });
 
-  
+    function updateH4 () {
+    if (stateNames.length > 0 && cityNames.length > 0) {
+      $(' .locations h4').text(stateNames + ', ' + cityNames);
+    } else {
+      $(' .locations h4').text(stateNames + cityNames);
+    }
+  }
+  const amenityIds = {};
+  $('.amenities .popover li input').change(function (event) {
+    if (this.checked) {
+      amenityIds[this.dataset.id] = this.dataset.name;
+    } else {
+      delete amenityIds[this.dataset.id];
+    }
+    const names = Object.values(amenityIds);
+    $('.amenities h4').text(names.join(', '));
+  });
